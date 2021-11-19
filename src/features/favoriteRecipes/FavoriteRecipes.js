8d462c608c6134827dfe2a9066a6af8ab398778c
Tojo Rabemananjara <trabema1@terpmail.umd.edu>
2021-11-18 19:27:03 -0500

    initial commit

---
+++ src/features/favoriteRecipes/FavoriteRecipes.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import FavoriteButton from "../../components/FavoriteButton";
+import Recipe from "../../components/Recipe";
+
+// Import removeRecipe from favoriteRecipesSlice.js
+import { removeRecipe } from './favoriteRecipesSlice.js';
+const unfavoriteIconUrl = 'https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/unfavorite.svg'
+
+export const FavoriteRecipes = (props) =>{
+  
+  // Extract favoriteRecipes and dispatch from props.
+  const { favoriteRecipes, dispatch } = props;
+  
+  const onRemoveRecipeHandler = (recipe) => {
+    // Dispatch a removeRecipe() action.
+    dispatch(removeRecipe(recipe));
+  };
+
+  // Map the recipe objects in favoriteRecipes to render <Recipe /> components.
+  return (
+    <div className="recipes-container">
+      {favoriteRecipes.map(createRecipeComponent)}
+    </div>
+  );
+
+  // Helper Function
+  function createRecipeComponent(recipe) {
+    return (
+      <Recipe recipe={recipe} key={recipe.id}>
+        <FavoriteButton
+          onClickHandler={() => onRemoveRecipeHandler(recipe)}
+          icon={unfavoriteIconUrl}
+        >
+          Remove Favorite
+        </FavoriteButton>
+      </Recipe>
+    )
+  }
+  
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { App } from './app/App.js';
+import { store } from './app/store.js';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const render = () => {
+  ReactDOM.render(
+    <App 
+      state={store.getState()}
+      dispatch={store.dispatch}
+    />,
+    document.getElementById('root')
+  )
+}
+store.subscribe(render);
+render();
